67430f7c8d92a89f42b25474ea2ceebecb3ee800
zhangkai123 <176776005@qq.com>
2012-11-21 23:25:45 +0800

    add pagination function

--- MeiRong/MuyingYongpin/DataController.m
+++ MeiRong/MuyingYongpin/DataController.m
@@ -38,31 +38,12 @@
         return;
     }
     dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
-//        NSMutableDictionary *params = [[NSMutableDictionary alloc] init];
-//        
-//        [params setObject:@"title,pic_url,click_url" forKey:@"fields"];
-//        [params setObject:@"50010788" forKey:@"cid"];
-//        [params setObject:@"taobao.taobaoke.items.get" forKey:@"method"];
-//        [params setObject:@"32217399" forKey:@"pid"];
-//        [params setObject:[NSString stringWithFormat:@"%d",pageN] forKey:@"page_no"];
-//        [params setObject:@"20" forKey:@"page_size"];
-//        [params setObject:@"5goldencrown" forKey:@"start_credit"];
-//        [params setObject:@"100" forKey:@"start_price"];
-//        [params setObject:@"2000" forKey:@"end_price"];
-//        [params setObject:@"commissionNum_desc" forKey:@"sort"];
-//        [params setObject:@"true" forKey:@"overseas_item"];
-//        [params setObject:@"true" forKey:@"mall_item"];
-//        [params setObject:@"true" forKey:@"is_mobile"];
-//        
-//        NSData *resultData=[Utility getResultData:params];
-//        [params release];
-//        NSMutableArray *pArray = [self parseProductsData:resultData];
         
         NSError *error;
         NSURLResponse *theResponse;
-        NSMutableURLRequest *theRequest=[NSMutableURLRequest requestWithURL:[NSURL URLWithString:@"http://192.168.1.105/~zhangkai/PinPHP_V2.21/fetchProducts.php"]];
+        NSMutableURLRequest *theRequest=[NSMutableURLRequest requestWithURL:[NSURL URLWithString:@"http://192.168.1.107/~zhangkai/PinPHP_V2.21/fetchProducts.php"]];
         [theRequest setHTTPMethod:@"POST"];
-        NSString *postString = @"&catId=434";
+        NSString *postString = [NSString stringWithFormat:@"catId=434&pageNumber=%d",pageN];  
         [theRequest setHTTPBody:[postString dataUsingEncoding:NSUTF8StringEncoding]];
         
         [theRequest addValue:@"application/x-www-form-urlencoded" forHTTPHeaderField:@"Content-Type"];
