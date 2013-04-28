8e8449a15ff1f1eff551090aa07d4c9ccd3f98d0
zhangkai123 <176776005@qq.com>
2013-04-28 20:11:55 +0800

    finish the product detail design

--- MeiRong/MuyingYongpin/TheBrandDetailViewController.m
+++ MeiRong/MuyingYongpin/TheBrandDetailViewController.m
@@ -67,14 +67,14 @@
     CGRect screenBounds = [[UIScreen mainScreen] bounds];
     if (screenBounds.size.height == 568) {
         // code for 4-inch screen
-        theTableView = [[UITableView alloc]initWithFrame:CGRectMake(0, 0, self.view.frame.size.width, 548) style:UITableViewStylePlain];
+        theTableView = [[UITableView alloc]initWithFrame:CGRectMake(0, 0, self.view.frame.size.width, 548 - 45) style:UITableViewStylePlain];
     } else {
         // code for 3.5-inch screen
-        theTableView = [[UITableView alloc]initWithFrame:CGRectMake(0, 0, self.view.frame.size.width, 460) style:UITableViewStylePlain];
+        theTableView = [[UITableView alloc]initWithFrame:CGRectMake(0, 0, self.view.frame.size.width, 460 - 45) style:UITableViewStylePlain];
     }
     firstCellHeight = smallImage.size.height * 320 / smallImage.size.width;
 
-    theTableView.backgroundColor = [UIColor clearColor];
+//    theTableView.backgroundColor = [UIColor clearColor];
     [theTableView setSeparatorStyle:UITableViewCellSeparatorStyleNone];
     theTableView.showsVerticalScrollIndicator = NO;
     theTableView.delegate = self;
@@ -82,72 +82,7 @@
     theTableView.clipsToBounds = NO;
     [self.view addSubview:theTableView];
     
-//    __block UITableView *weaktheTalbleView = theTableView;
-    [theTableView addInfiniteScrollingWithActionHandler:^{
-    
-//        [weaktheTalbleView.infiniteScrollingView performSelector:@selector(stopAnimating) withObject:nil afterDelay:0];
-    }];
-
-    self.view.backgroundColor = [UIColor colorWithPatternImage:[UIImage imageNamed:@"SheetBackground"]];
-            
-//    realBackView = [[UIView alloc]initWithFrame:CGRectMake(0, firstCellHeight - 20 - 45 - 60, 320, 60)];
-//    realBackView.backgroundColor = [UIColor blackColor];
-//    [realBackView setAlpha:0.5];
-//    [self.view addSubview:realBackView];
-//    
-//    titleLabel = [[UILabel alloc]initWithFrame:CGRectMake(40, realBackView.frame.origin.y + 15, 285, 30)];
-//    titleLabel.backgroundColor = [UIColor clearColor];
-//    [titleLabel setFont:[UIFont systemFontOfSize:14]];
-//    [titleLabel setTextColor:[UIColor whiteColor]];
-//    titleLabel.text = product.title;
-//    [self.view addSubview:titleLabel];
-//    
-//    shopView = [[UIView alloc]initWithFrame:CGRectMake(320 - 145, firstCellHeight - 20 - 45 - 60 - 20, 145, 40)];
-//    shopView.backgroundColor = [UIColor clearColor];
-//    [self.view addSubview:shopView];
-//    
-//     UIImageView *shopImageView = [[UIImageView alloc]initWithFrame:CGRectMake(0, 0, 145, 40)];
-//    shopImageView.userInteractionEnabled = YES;
-//    shopImageView.backgroundColor = [UIColor colorWithRed:1 green: 0.6 blue:0.8 alpha:1];
-//    shopImageView.alpha = 0.8;
-//    [shopView addSubview:shopImageView];
-//    [shopImageView release];
-//    
-//     UILabel *shopLabel = [[UILabel alloc]initWithFrame:CGRectMake(0, 0, 145, 40)];
-//    shopLabel.backgroundColor = [UIColor clearColor];
-//    [shopLabel setText:[NSString stringWithFormat:@"%@ | 查看详情",product.price]];
-//    [shopLabel setTextColor:[UIColor whiteColor]];
-//    [shopLabel setTextAlignment:NSTextAlignmentCenter];
-//    [shopView addSubview:shopLabel];
-//    [shopLabel release];
-//    
-//    UIButton *shopButton = [[UIButton alloc]initWithFrame:CGRectMake(0, 0, 145, 40)];
-//    [shopButton addTarget:self action:@selector(buyProduct) forControlEvents:UIControlEventTouchUpInside];
-//    [shopView addSubview:shopButton];
-//    [shopButton release];
-//    
-//    UIView *footerView = [[UIView alloc]initWithFrame:CGRectMake(0, firstCellHeight - 20 - 45, 320, 45)];
-//    footerView.backgroundColor = [UIColor blackColor];
-//    [footerView setAlpha:0.7];
-//    [self.view addSubview:footerView];
-//    [footerView release];
-//    
-//    collectButton = [[UIButton alloc]initWithFrame:CGRectMake(70, firstCellHeight - 20 - 45, 45, 45)];
-//    [collectButton addTarget:self action:@selector(collectProduct) forControlEvents:UIControlEventTouchUpInside];
-//    [collectButton setImage:[UIImage imageNamed:@"ico_footer_like"] forState:UIControlStateNormal];
-//    [self.view addSubview:collectButton];
-//    if (product.collect) {
-//        [collectButton setImage:[UIImage imageNamed:@"ico_footer_like_active"] forState:UIControlStateNormal];
-//    }else{
-//        [collectButton setImage:[UIImage imageNamed:@"ico_footer_like"] forState:UIControlStateNormal];
-//    }
-//    self.collection = product.collect;
-//
-//    UIButton *shareButton = [[UIButton alloc]initWithFrame:CGRectMake(205, firstCellHeight - 20 - 45, 45, 45)];
-//    [shareButton addTarget:self action:@selector(shareProduct) forControlEvents:UIControlEventTouchUpInside];
-//    [shareButton setImage:[UIImage imageNamed:@"ico_footer_share"] forState:UIControlStateNormal];
-//    [self.view addSubview:shareButton];
-//    [shareButton release];
+    self.view.backgroundColor = [UIColor colorWithPatternImage:[UIImage imageNamed:@"SheetBackground"]];            
 }
 - (void)viewDidAppear:(BOOL)animated
 {
@@ -161,40 +96,15 @@
 //        [dataController featchProductDetail:product.num_id theProduct:product];
 //    }
 //}
-//- (void)scrollViewDidScroll:(UIScrollView *)scrollView
-//{
-//    if (scrollView.contentOffset.y >= 65) {
-//        if (![realBackView isDescendantOfView:theTableView]) {
-//            [realBackView removeFromSuperview];
-//            [titleLabel removeFromSuperview];
-//            realBackView.frame = CGRectMake(0, firstCellHeight - 60, realBackView.frame.size.width, realBackView.frame.size.height);
-//            titleLabel.frame = CGRectMake(30, realBackView.frame.origin.y + 15, 280, 30);
-//            [theTableView addSubview:realBackView];
-//            [theTableView addSubview:titleLabel];
-//            [self.view bringSubviewToFront:shopView];
-//        }
-//    }
-//    if (scrollView.contentOffset.y <= 65) {
-//        if ([realBackView isDescendantOfView:theTableView]) {
-//            [realBackView removeFromSuperview];
-//            [titleLabel removeFromSuperview];
-//            realBackView.frame = CGRectMake(0, firstCellHeight - 20 - 45 - 60, 320, 60);
-//            titleLabel.frame = CGRectMake(30, realBackView.frame.origin.y + 15, 280, 30);
-//            [self.view addSubview:realBackView];
-//            [self.view addSubview:titleLabel];
-//            [self.view bringSubviewToFront:shopView];
-//        }
-//    }
-//}
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
 {
-    return 2;
+    return 1;
 }
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
     if (section == 0) {
         
-        return 1;
+        return 5;
     }else{
      
         return [self.product.imagesArray count];
@@ -205,7 +115,25 @@
     float rowHeight = 0;
     if (indexPath.section == 0) {
         
-        rowHeight = firstCellHeight;
+        switch (indexPath.row) {
+            case 0:
+                rowHeight = firstCellHeight;
+                break;
+            case 1:
+                rowHeight = 60;
+                break;
+            case 2:
+                rowHeight = 30;
+                break;
+            case 3:
+                rowHeight = 45;
+                break;
+            case 4:
+                rowHeight = 100;
+                break;
+            default:
+                break;
+        }
     }else{
         NSDictionary *imageDic = [self.product.imagesArray objectAtIndex:indexPath.row];
         rowHeight = [[imageDic objectForKey:@"imageHeight"] floatValue];
@@ -216,12 +144,60 @@
 {
     UITableViewCell *theCell = nil;
     if (indexPath.section == 0) {
-        FirstCell *firstImageCell = nil;
-        if (!theCell) {
-            firstImageCell = [[[FirstCell alloc]initWithStyle:UITableViewCellStyleDefault reuseIdentifier:nil imageHeight:firstCellHeight]autorelease];
+        switch (indexPath.row) {
+            case 0:
+            {
+                FirstCell *firstImageCell = nil;
+                if (!theCell) {
+                    firstImageCell = [[[FirstCell alloc]initWithStyle:UITableViewCellStyleDefault reuseIdentifier:nil imageHeight:firstCellHeight]autorelease];
+                }
+                [firstImageCell.myImageView setImageWithURL:[NSURL URLWithString:product.pic_url] placeholderImage:self.smallImage];
+                theCell = firstImageCell;
+            }
+                break;
+            case 1:
+            {
+                UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"cell1"];
+                if (!cell) {
+                    cell = [[[UITableViewCell alloc]initWithStyle:UITableViewCellStyleDefault reuseIdentifier:@"cell1"]autorelease];
+                }
+                cell.textLabel.text = @"product detail";
+                theCell = cell;
+            }
+                break;
+            case 2:
+            {
+                UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"cell2"];
+                if (!cell) {
+                    cell = [[[UITableViewCell alloc]initWithStyle:UITableViewCellStyleDefault reuseIdentifier:@"cell2"]autorelease];
+                }
+                cell.textLabel.text = @"store detail";
+                theCell = cell;
+            }
+                break;
+            case 3:
+            {
+                UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"cell3"];
+                if (!cell) {
+                    cell = [[[UITableViewCell alloc]initWithStyle:UITableViewCellStyleDefault reuseIdentifier:@"cell3"]autorelease];
+                }
+                cell.textLabel.text = @"collect and share";
+                theCell = cell;
+            }
+                break;
+            case 4:
+            {
+                UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"cell4"];
+                if (!cell) {
+                    cell = [[[UITableViewCell alloc]initWithStyle:UITableViewCellStyleDefault reuseIdentifier:@"cell4"]autorelease];
+                }
+                cell.textLabel.text = @"relate images";
+                theCell = cell;
+            }
+                break;
+            default:
+                break;
         }
-        [firstImageCell.myImageView setImageWithURL:[NSURL URLWithString:product.pic_url] placeholderImage:self.smallImage];
-        theCell = firstImageCell;
     }else{
         DetailImageCell *imageCell = nil;
         if (!theCell) {
